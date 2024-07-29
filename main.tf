90d740e2920181eb2a3054a89191613af0dbf412
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-29 13:32:18 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,64 @@
 # Modules
 module "alb" {
-  source = "./alb"
+  source = "./modules/alb"
 
   # ...
 }
 
 module "asg" {
-  source = "./asg"
+  source = "./modules/asg"
 
   # ...
 }
 
 module "ec2" {
-  source = "./ec2"
+  source = "./modules/ec2"
 
   # ...
 }
 
 module "iam-policy" {
-  source = "./iam-policy"
+  source = "./modules/iam-policy"
 
   # ...
 }
 module "rds" {
-  source = "./rds"
+  source = "./modules/rds"
 
   # ...
 }
 module "security-group" {
-  source = "./security-group"
+  source = "./modules/security-group"
 
   # ...
 }
 module "ses" {
-  source = "./ses"
+  source = "./modules/ses"
 
   # ...
 }
 module "tag-policy" {
-  source = "./tag-policy"
+  source = "./modules/tag-policy"
 
   # ...
 }
 module "vpc" {
-  source = "./vpc"
+  source = "./modules/vpc"
 
   # ...
 }
 module "dev_vpc" {
-  source = "./environments/dev/vpc"
+  source = "./modules/environments/dev/vpc"
 
   # ...
 }
 module "alb_asg" {
-  source = "./environments/dev/alb-asg"
+  source = "./modules/environments/dev/alb-asg"
 
   # ...
 }
 module "ec2" {
-  source = "./environments/dev"
+  source = "./modules/environments/dev"
 
   # ...
 }
