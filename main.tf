ea2ed1a10a19cedb123fa77c7ae9d40d4c97b2ee
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-29 13:27:26 -0500

    updated main.tf and created backup folder

--- main.tf
+++ main.tf
@@ -1,6 +1,68 @@
 # Modules
-module "create_two_tier_aws" {
-  source = "./modules"
+module "alb" {
+  source = "./alb"
 
-  
+  # ...
 }
+
+module "asg" {
+  source = "./asg"
+
+  # ...
+}
+
+module "ec2" {
+  source = "./ec2"
+
+  # ...
+}
+
+module "iam-policy" {
+  source = "./iam-policy"
+
+  # ...
+}
+module "rds" {
+  source = "./rds"
+
+  # ...
+}
+module "security-group" {
+  source = "./security-group"
+
+  # ...
+}
+module "ses" {
+  source = "./ses"
+
+  # ...
+}
+module "tag-policy" {
+  source = "./tag-policy"
+
+  # ...
+}
+module "vpc" {
+  source = "./vpc"
+
+  # ...
+}
+module "dev_vpc" {
+  source = "./environments/dev/vpc"
+
+  # ...
+}
+module "alb_asg" {
+  source = "./environments/dev/alb-asg"
+
+  # ...
+}
+module "ec2" {
+  source = "./environments/dev"
+
+  # ...
+}
+
+
+
+
