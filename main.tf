218983c4a875545f05e26ae1c92eac2be7b4da32
chiptranz-int <69421097+chiptranz-int@users.noreply.github.com>
2024-07-30 16:42:57 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -308,6 +308,20 @@ module "vpc" {
 }
 module "acm" {
   source = "./environments/dev/acm"
+  region = "us-west-2"
+
+  domain_name                                 = "prom.devopsproject.dev"
+  validation_method                           = "DNS"
+  key_algorithm                               = "RSA_2048"
+  certificate_transparency_logging_preference = "ENABLED"
+  dns_domain_name                             = "devopsproject.dev"
+
+  # Tag Keys
+  name        = ""
+  owner       = "techiescamp"
+  environment = "dev"
+  cost_center = "techiescamp-commerce"
+  application = "acm"
 
   # ...
 }
